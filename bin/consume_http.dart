8041142c170c7b011956abccc0d041735b8a9043
Andrius Verde <andriusverde@airspace.com>
2023-06-28 09:56:08 -0600

    Adding project

---
+++ bin/consume_http.dart
@@ -0,0 +1,5 @@
+import 'package:consume_http/consume_http.dart' as consume_http;
+
+void main(List<String> arguments) {
+  print('Hello world: ${consume_http.calculate()}!');
+}
---
+++ test/consume_http_test.dart
@@ -0,0 +1,8 @@
+import 'package:consume_http/consume_http.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
